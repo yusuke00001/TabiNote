ef13f9c4e29faa3e9fceafb0fad9eea042effa69
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-04-16 18:10:25 +0900

    機能修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    flash[:notice] = ログインしました
+    flash[:notice] = "ログインしました"
     homes_path
   end
 
