2d423a24cb91e3961c2c12533b8f09c637e539b2
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-04-17 16:52:09 +0900

    進行中と過去のしおりの条件分岐を作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @trips = Trip.where(user_id: current_user.id)
+    @trips_in_progress = Trip.where(user_id: current_user.id, status: :in_progress)
+    @trips_past = Trip.where(user_id: current_user.id, status: :complited)
   end
 end
