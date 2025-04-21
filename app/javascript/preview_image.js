5b67e9ad4e078043465aa14125d8b424e4ef3828
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-04-21 12:16:07 +0900

    jsをapp/javascriptに記述

---
+++ app/javascript/preview_image.js
@@ -0,0 +1,17 @@
+
+function loadImage(obj) {
+  const allPreview = document.getElementById('post_avatar');
+  const newPreview = document.createElement("p");
+  allPreview.querySelector("p").remove();
+  newPreview.setAttribute("id","preview");
+  allPreview.insertBefore(newPreview, allPreview.firstChild);
+  const fileReader = new FileReader();
+      fileReader.onload = (function (e) {
+        const img = new Image();
+        img.src = e.target.result;
+        img.classList.add("preview-avatar")
+        document.getElementById('preview').appendChild(img);
+      });
+    fileReader.readAsDataURL(obj.files[0]);
+}
+window.loadImage = loadImage;
