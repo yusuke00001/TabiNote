f47aa87f08719bce3fe49187f53ae52591e3d6f5
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-04-24 14:57:14 +0900

    修正

--- app/models/trip_user.rb
+++ app/models/trip_user.rb
@@ -1,13 +1,7 @@
 class TripUser < ApplicationRecord
-  after_create :trip_users_create
   belongs_to :user
   belongs_to :trip
 
   enum :host, { member: 0, leader: 1 }
   validates :host, presence: true
-
-  def trip_users_create
-    binding.pry
-    create(trip_id: trip_id, user_id: user_id, host: :leader)
-  end
 end
