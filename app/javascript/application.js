9aca66b0b3a280f8979302834c823cdc6deaaba9
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-05-11 18:11:29 +0900

    提案されたスポットの削除機能を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "preview_image"
+import Rails from "@rails/ujs"
+Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "preview_image", to: "preview_image.js"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
