18e773aa10f49bc1f2f0917faeae1a1dca723362
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-05-12 12:06:52 +0900

    ログインしないとアクセスできないように制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
