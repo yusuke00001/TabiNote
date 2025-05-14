c15e7ba36976e76755c4b6f9978b81a759a3fe23
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-05-14 12:01:21 +0900

    spot_suggestionsコントローラの修正

--- app/controllers/spot_suggestions_controller.rb
+++ app/controllers/spot_suggestions_controller.rb
@@ -1,26 +1,23 @@
 class SpotSuggestionsController < ApplicationController
   def create
     spot_suggestion = SpotSuggestion.new(spot_suggestions_params)
-    trip = Trip.find(params[:trip_id])
-    spot = Spot.find(params[:spot_suggestion][:spot_id])
     if spot_suggestion.save
       flash[:notice] = "スポットの提案に成功しました"
-      redirect_to trip_path(trip)
+      redirect_to trip_path(spot_suggestion.trip_id)
     else
       flash[:alert] = "スポットの提案に失敗しました"
-      redirect_to trip_spot_path(spot, trip)
+      redirect_back fallback_location: trip_spots_path(spot_suggestion.trip_id)
     end
   end
 
   def destroy
-    spot_suggestion = SpotSuggestion.find_by(trip_id: params[:id], spot_id: params[:spot_id])
-    trip = Trip.find(params[:id])
+    spot_suggestion = SpotSuggestion.find_by!(trip_id: params[:id], spot_id: params[:spot_id])
     if spot_suggestion.delete
       flash[:notice] = "スポットの削除が完了しました"
-      redirect_to trip_path(trip)
+      redirect_to trip_path(spot_suggestion.trip_id)
     else
       flash[:alert] = "スポットの削除に失敗しました"
-      redirect_to trip_path(trip)
+      redirect_back fallback_location: trip_path(spot_suggestion.trip_id)
     end
   end
 
