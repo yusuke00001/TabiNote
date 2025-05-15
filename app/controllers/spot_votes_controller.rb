64c547bcbf282dd444d601caf56a18e59cf3f367
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-05-15 15:27:38 +0900

    投票機能の実装

--- app/controllers/spot_votes_controller.rb
+++ app/controllers/spot_votes_controller.rb
@@ -1,9 +1,9 @@
 class SpotVotesController < ApplicationController
   def create
-    suggestion_ids = params[:spot_suggestion_ids]
-    suggestion_ids.each do |suggestion_id|
-      SpotVote.create!(trip_id: params[:trip_id], spot_suggestion_id: suggestion_id, user_id: current_user.id)
-    end
+      suggestion_ids = params[:spot_suggestion_ids]
+      suggestion_ids.each do |suggestion_id|
+        SpotVote.create!(trip_id: params[:trip_id], spot_suggestion_id: suggestion_id, user_id: current_user.id)
+      end
     flash[:notice] = "投票しました"
     redirect_to trip_path(params[:trip_id])
   end
