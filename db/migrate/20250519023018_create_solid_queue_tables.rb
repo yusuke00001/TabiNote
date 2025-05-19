dd5949015d3d8e496552a6b199d85206f48e6dac
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-05-19 11:51:32 +0900

    a

--- db/migrate/20250519023018_create_solid_queue_tables.rb
+++ db/migrate/20250519023018_create_solid_queue_tables.rb
@@ -1,17 +1,19 @@
 class CreateSolidQueueTables < ActiveRecord::Migration[8.0]
   def change
     create_table :solid_queue_jobs do |t|
-      t.string :queue_name, null: false
-      t.string :job_class, null: false
-      t.datetime :scheduled_at, precision: nil
-      t.datetime :finished_at, precision: nil
-      t.text :arguments
-      t.integer :priority, default: 0, null: false
+      t.string     :queue_name, null: false
+      t.string     :job_class, null: false
+      t.text       :arguments
+      t.text       :priority
+      t.datetime   :scheduled_at
+      t.datetime   :finished_at
+      t.string     :active_job_id
+      t.integer    :attempts, null: false, default: 0
       t.timestamps
     end
 
     add_index :solid_queue_jobs, :queue_name
     add_index :solid_queue_jobs, :scheduled_at
-    add_index :solid_queue_jobs, :finished_at
+    add_index :solid_queue_jobs, :active_job_id
   end
 end
