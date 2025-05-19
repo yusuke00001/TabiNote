387101cd1b22fcec7423c4ece8c691e3022e2259
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-05-19 11:36:24 +0900

    修正

--- db/migrate/20250519023018_create_solid_queue_tables.rb
+++ db/migrate/20250519023018_create_solid_queue_tables.rb
@@ -1,5 +1,6 @@
-class CreateSolidQueueTables < ActiveRecord::Migration[8.0]
+class CreateSolidQueueTables < ActiveRecord::Migration[7.1] # ← Railsのバージョンに応じて
   def change
-    SolidQueue::Migration.new.change
+    require "solid_queue/database/schema"
+    SolidQueue::Database::Schema.new(self).change
   end
 end
