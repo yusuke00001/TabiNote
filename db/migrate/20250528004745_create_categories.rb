ab1c278c0f84cb87d6326620a7d496585b6fed32
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-05 09:49:29 +0900

    初期値の修正

--- db/migrate/20250528004745_create_categories.rb
+++ db/migrate/20250528004745_create_categories.rb
@@ -5,11 +5,5 @@ class CreateCategories < ActiveRecord::Migration[8.0]
       t.integer :stay_time
       t.timestamps
     end
-    Category.create!(name: "sightseeing", stay_time: 90)
-    Category.create!(name: "shopping", stay_time: 60)
-    Category.create!(name: "leisure_land", stay_time: 300)
-    Category.create!(name: "nature", stay_time: 50)
-    Category.create!(name: "restaurant", stay_time: 60)
-    Category.create!(name: "other")
   end
 end
