5a853af7842ec57680516256aa7a05600e3643af
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-16 17:11:37 +0900

    修正

--- db/migrate/20250612080343_add_decided_plan_id_to_trips.rb
+++ db/migrate/20250612080343_add_decided_plan_id_to_trips.rb
@@ -1,6 +1,6 @@
 class AddDecidedPlanIdToTrips < ActiveRecord::Migration[8.0]
   def change
     add_column :trips, :decided_plan_id, :bigint
-    add_foreign_key :trips, :plans, column: :decided_plan_id, on_update: :cascade, on_delete: :cascade
+    add_foreign_key :trips, :plans, column: :decided_plan_id, on_update: :cascade
   end
 end
