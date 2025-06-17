c7d9b45d309a1299b7437baeb5dfc3d131ba680b
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-17 11:02:19 +0900

    tripsコントローラの無駄なインスタンス変数を削除しました

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -36,29 +36,23 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
     @trip_users = @trip.trip_users.order(is_leader: :desc)
-    @spot_suggestions = @trip.spot_suggestions
     spot_votes = @trip.spot_votes.pluck(:spot_suggestion_id)
     ng_spot = spot_votes.group_by { |s| s }.select { |_, value| value.size >= 2 }.keys
-    @voted_result = @spot_suggestions.reject { |spot| ng_spot.include?(spot.id) }
+    @voted_result = @trip.spot_suggestions.reject { |spot| ng_spot.include?(spot.id) }
     @plan = Plan.find_by(id: @trip.decided_plan_id)
-    if @plan.present?
-      @spots = @plan.spots
-    end
   end
 
   def suggestion
-    @trip = Trip.find(params[:id])
-    @spot_suggestions = @trip.spot_suggestions
-    render partial: "trips/suggestion", locals: { trip: @trip, spot_suggestions: @spot_suggestions }
+    trip = Trip.find(params[:id])
+    render partial: "trips/suggestion", locals: { trip: trip, spot_suggestions: trip.spot_suggestions }
   end
 
   def vote
-    @trip = Trip.find(params[:id])
-    @spot_suggestions = @trip.spot_suggestions
-    voted_spot_suggestion_ids = SpotVote.where(trip_id: @trip, user_id: current_user.id).pluck(:spot_suggestion_id)
-    @voted_spot_suggestions = @spot_suggestions.select { |s| voted_spot_suggestion_ids.include?(s.id) }
-    @not_voted_spot_suggestions = @spot_suggestions.reject { |s| voted_spot_suggestion_ids.include?(s.id) }
-    render partial: "trips/vote", locals: { trip: @trip, spot_suggestions: @spot_suggestions, voted_spot_suggestions: @voted_spot_suggestions, not_voted_spot_suggestions: @not_voted_spot_suggestions }
+    trip = Trip.find(params[:id])
+    voted_spot_suggestion_ids = SpotVote.where(trip_id: trip.id, user_id: current_user.id).pluck(:spot_suggestion_id)
+    voted_spot_suggestions = trip.spot_suggestions.select { |s| voted_spot_suggestion_ids.include?(s.id) }
+    not_voted_spot_suggestions = trip.spot_suggestions.reject { |s| voted_spot_suggestion_ids.include?(s.id) }
+    render partial: "trips/vote", locals: { trip: trip, spot_suggestions: trip.spot_suggestions, voted_spot_suggestions: voted_spot_suggestions, not_voted_spot_suggestions: not_voted_spot_suggestions }
   end
 
   private
