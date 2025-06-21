5896552a6e785832adca425b52ff9bbfc8072f9c
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-21 18:39:11 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ require "csv"
 Category.create(name: "sightseeing", stay_time: 90)
 Category.create(name: "shopping", stay_time: 60)
 Category.create(name: "leisure_land", stay_time: 300)
-Category.create(name: "nature", stay_time: 50)
+Category.create(name: "nature", stay_time: 45)
 Category.create(name: "restaurant", stay_time: 60)
 Category.create(name: "other", stay_time: 60)
 
