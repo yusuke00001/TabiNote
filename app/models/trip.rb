d84f1a5a6dd140597d0b5cab5b4b2e021d28e1d1
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-23 18:28:49 +0900

    修正

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,14 @@
 class Trip < ApplicationRecord
+  DESTINATIONS = [
+    "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+    "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
+    "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県",
+    "岐阜県", "静岡県", "愛知県", "三重県",
+    "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+    "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+    "徳島県", "香川県", "愛媛県", "高知県",
+    "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
+  ]
   after_create :create_trip_user
 
   has_many :trip_transportations
@@ -18,17 +28,7 @@ class Trip < ApplicationRecord
   validates :spot_vote_limit, presence: true
   validates :start_time, presence: true
   validates :finish_time, presence: true
-
-  DESTINATION = [
-    "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
-    "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
-    "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県",
-    "岐阜県", "静岡県", "愛知県", "三重県",
-    "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
-    "鳥取県", "島根県", "岡山県", "広島県", "山口県",
-    "徳島県", "香川県", "愛媛県", "高知県",
-    "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
-  ]
+  validates :destination, inclusion: { in: DESTINATIONS }
 
   def create_trip_user
     TripUser.create!(trip_id: id, user_id: created_user_id, is_leader: true)
