c45dcfe1b1e49b3e9324832f7836cf9a27278c80
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-24 16:33:21 +0900

    fix/trip_create

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
+  before_create :trip_image
   DESTINATIONS = [
     "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
     "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
