09b1b792cce7498eca6d21bc658174f6ef9e1b9e
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-24 14:43:15 +0900

    DB内検索に切り替えるために検索時にキーワードと地名を保存できるように修正

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_12_080343) do
 
   create_table "keywords", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "word", null: false
+    t.string "location_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
