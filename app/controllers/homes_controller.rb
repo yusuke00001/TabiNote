0cbbc89bbce50871aba5c6aa39a85b08ddad808b
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-25 21:50:51 +0900

    修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @trips_in_progress_first_row = current_user.trips_in_progress_first_row
-    @trips_in_progress_other_row = current_user.trips_in_progress_other_row
+    @trips_in_progress = current_user.trips_in_progress
     @trips_past = current_user.trips_past
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,8 @@ class User < ApplicationRecord
     avatar
   end
 
-  def trips_in_progress_first_row
-    trips.where(status: :in_progress).limit(3)
-  end
-
-  def trips_in_progress_other_row
-    trips.where(status: :in_progress).offset(3)
+  def trips_in_progress
+    trips.where(status: :in_progress)
   end
 
   def trips_past
