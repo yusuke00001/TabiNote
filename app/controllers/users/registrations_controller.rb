7bfc2e04f8a0a1eec42fbbbacbe3143760fc5229
ゆうすけ <yuusuke@naitouyuusukenoMacBook-Air.local>
2025-06-26 08:37:33 +0900

    招待リンクの仕組みを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    homes_path
+    stored_location_for(resource) || homes_path
   end
 
   def after_update_path_for(resource)
